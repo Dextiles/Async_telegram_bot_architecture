17b8aa17feddc8f85f92109b0a0f97cdaafadf53
Ivan <Dextiles@outlook.com>
2025-03-04 19:32:34 +0300

    set_default_commands function added docstring and annotation

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -1,8 +1,14 @@
 from telebot.types import BotCommand
 from config_data.config import DEFAULT_COMMANDS
+from telebot.async_telebot import AsyncTeleBot
 
 
-async def set_default_commands(bot):
+async def set_default_commands(bot: AsyncTeleBot) -> None:
+    """
+    Устанавливает команды по умолчанию для телеграм-бота.
+
+    :param bot: Экземпляр телеграм-бота.
+    """
     await bot.set_my_commands(
         [BotCommand(*i) for i in DEFAULT_COMMANDS]
     )
