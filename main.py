7d5156d951c8b7de96ed210b0a19ba33847021b4
Ivan <Dextiles@outlook.com>
2025-03-04 19:30:58 +0300

    main.py added docstring and annotation

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from utils.set_bot_commands import set_default_commands
 import asyncio
 
 
-async def main():
+async def main() -> None:
+    """
+    Основная асинхронная функция для запуска телеграм-бота.
+
+    Функция устанавливает команды по умолчанию для бота и запускает бесконечное опросное взаимодействие.
+    """
     await set_default_commands(bot)
     await bot.infinity_polling()
 
